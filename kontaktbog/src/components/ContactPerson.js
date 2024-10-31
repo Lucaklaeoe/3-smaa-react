6c49554b39044d8655cc2780276efd72062f9f20
Emil <emilloengaa@icloud.com>
2024-10-31 11:08:56 +0100

    Sejhed

--- kontaktbog/src/components/ContactPerson.js
+++ kontaktbog/src/components/ContactPerson.js
@@ -1,30 +1,24 @@
-import Bullit from './Bullit';
-
-function ContactPerson() {
-
-    const data =[
-        {id: 1, message: 'Kontakt Person 1'},
-        {id: 2, message: 'Kontakt Person 2'},
-        {id: 3, message: 'Kontakt Person 3'},
-        {id: 4, message: 'Kontakt Person 4'},
-        {id: 5, message: 'Kontakt Person 5'},
-        {id: 6, message: 'Kontakt Person 6'},
-    ]
-    const h1Style = {
-        color: 'White',
-        fontSize: '1.5em',
-        marginTop: '20px',
+function ContactPerson(props) {
+    
+    const ContactPersonStyle = {
+        color: 'white',
+        fontSize: '1em',
+        marginTop: '10px',
         marginLeft: '50px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '15px',
     }
-    return (
-
-        <div>
-            <h1 style={h1Style}>Kontakt Person</h1>
-            {data.map(item =>
-                <Bullit message={item.message} id={item.id}/>
-            )}
-        </div>
-    );
+    return(
+        
+       <li style={ContactPersonStyle}>
+        <div><b>Fornavn:</b>{props.firstname} </div>
+        <div><b>Efternavn:</b>{props.lastname}:</div>
+        <div><b>E-mail:</b>{props.email}</div>
+        <div><b>Firma:</b>{props.firm}</div>
+        <div><b>Stilling:</b>{props.position}</div>
+        </li>
+    )
 }
 
 export default ContactPerson
--- kontaktbog/src/components/NewContact.js
+++ kontaktbog/src/components/NewContact.js
@@ -1,48 +1,91 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Modal from 'react-modal';
 
-function NewContact() {
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
-    const Style = {
-        marginBottom: '20px',
-        marginTop: '20px',
-        marginLeft: '50px',
-        display: 'flex',
-        flexDirection: 'column',
-        width: '350px',
-    }
-    const ButtonStyle = {
-        width:'100px',
-        marginBottom: '20px',
-    }
-    //get       //setændre
-    const [contactCount, setContactCount] = useState(0);
-    function handleAddcontact() {
+const formStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '15px',
+  padding: '10px',
+}
 
-      // alert("Kontakt tilføjet")
-      setContactCount(count => count + 1);
-      
-  }
-    return (
-      <form style={Style}>
-            <label>Fornavn:</label>
-            <input type="text" maxLength="20"/>
-            <label> Efternavn:</label>
-            <input type="text" maxLength="20"/>
-            <label> E-mail:</label>
-            <input type="text" maxLength="20"/>
-            <label> Tlf.:</label>
-            <input type="text" maxLength="20"/>
-            <label> Firma:</label>
-            <input type="text" maxLength="20"/>
-            <label> Position:</label>
-            <input type="text" maxLength="20"/>
+const buttonStyle = {
+  backgroundColor: '#ff9d88',
+  color: 'white',
+  border: 'none',
+  padding: '10px 20px',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  marginLeft: '20px',
+}
 
-            <p> Tilføj kontakt</p>
-            <button style={ButtonStyle} type="button" onClick={handleAddcontact}>Tilføj kontakt</button>
-            {/* <button onClick={() => alert("Add kontakt")}> Add kontakt</button> */}
-            Kontakter tilføjet: { contactCount.toString() }
-      </form>
-    )
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
+
+export default function MyModal() {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
   }
 
-  export default NewContact
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button style={buttonStyle} onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}></h2>
+        
+        <h1>TILFØJ NY KONTAKTPERSON</h1>
+        <form style={formStyle}> 
+          <label>
+            First name:
+            <input type="text" name="firstname" />
+          </label>
+          <label>
+            Last name:
+            <input type="text" name="lastname" />
+          </label>
+          <label>
+            Email:
+            <input type="email" name="email" />
+          </label>
+          <label>
+            Firm:
+            <input type="text" name="firm" />
+          </label>
+          <label>
+            Position:
+            <input type="text" name="position" />
+          </label>
+        </form>
+        <button onClick={closeModal}>Tilføj Kontakt</button>
+      </Modal>
+    </div>
+  );
+}
